43da0aa1ff0f24cc47560f6609be989896739109
Musppelheim <neloliver@hotmail.com>
2020-04-20 15:04:57 +0700

    Initial commit

---
+++ src/main/java/ru/netology/domain/AfishaManager.java
@@ -0,0 +1,16 @@
+package ru.netology.domain;
+
+public class AfishaManager {
+    Movies[] movies = {
+            new Movies(1,"Номер один", "comedy"),
+            new Movies(2, "Trolls:World trip","cartoon"),
+            new Movies(3, "Bloodshot", "action"),
+            new Movies(4, "Invisible Man", "horror"),
+            new Movies(5, "Отель Белград", "comdey"),
+            new Movies(6, "Onward", "cartoon"),
+            new Movies(7, "The Gentlemen", "action"),
+            new Movies(8, "Parasite", "triller/comedy"),
+            new Movies(9, "Sonic The Hedgehog", "cartoon"),
+            new Movies(10, "The Irishman","drama/crime")
+    };
+}
